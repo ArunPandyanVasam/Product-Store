1b77a12029ae1ab8e4b0cb0fcf20566c320252d5
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-02-17 16:30:19 -0500

    changed the url from /products to api/products in server.js file

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 // route
-app.post("/products", async (req, res) => {
+app.post("api/products", async (req, res) => {
     const product = req.body;   // user will send this data
 
     if (!product.name || !product.price || !product.image) {
@@ -23,7 +23,7 @@ app.post("/products", async (req, res) => {
     const newProduct = new Product(product);  
 
     try {
-        await newProduct.save(); // the them to database
+        await newProduct.save(); // save them to database
         res.status(201).json({ success: true, data: newProduct}); // 201 -> something created, data ->return new product
     } catch (error) {
         console.error("Error in create product:", error.message);
