ff96e3db91591b3082b77766eed7df05d07c8928
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-02-17 16:36:52 -0500

    middleware is added to take request from user and parse it to json into the body.

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ dotenv.config();
 const app = express(); 
 
 
+app.use(express.json()); // allows us to accept JSON data in the body
+
 // route
 app.post("/api/products", async (req, res) => {
     const product = req.body;   // user will send this data
