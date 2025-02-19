162aa8883f9f56d20ab90bb4ef851ba86bcd075a
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-02-18 20:30:56 -0500

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
 
-export default HomePage
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+
+        </SimpleGrid>
+
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          No products found 😊{" "}
+          <Link to={"/create"}>
+            <Text
+              as="span"
+              color="blue.500"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;
